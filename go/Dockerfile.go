d74cc42d961f3f038341453bf635cbf63dc4c3b1
Alex Spina <hello@alexspina.com>
2025-05-23 13:53:22 -0400

    fixes python dev container Dockerfile path, adds LABELs to Dockerfiles, adds...
    ...Go dev container image and adds stage to base image publish GitHub
    Action Workflow definition to make the images public.

---
+++ go/Dockerfile.go
@@ -0,0 +1,21 @@
+FROM ghcr.io/alexmspina/base-dev-container:0.0.2
+
+LABEL org.opencontainers.image.source=https://github.com/alexmspina/dev-containers
+LABEL org.opencontainers.image.description="Go 1.24.3 Development Container"
+
+# Install Go 1.24.3 and golangci-lint
+RUN curl -LO https://go.dev/dl/go1.24.3.linux-arm64.tar.gz && \
+    rm -rf /usr/local/go && \
+    tar -C /usr/local -xzf go1.24.3.linux-arm64.tar.gz && \
+    rm go1.24.3.linux-arm64.tar.gz
+
+ENV PATH="/usr/local/go/bin:${PATH}"
+ENV GOPATH="/go"
+ENV PATH="${GOPATH}/bin:${PATH}"
+
+RUN curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b /usr/local/bin v1.59.0
+
+WORKDIR /app
+
+SHELL ["/usr/bin/zsh", "-c"]
+CMD ["zsh"]
