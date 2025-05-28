5f282b29eab2aff0368525c3bf0dead987c7bb4d
Alex Spina <hello@alexspina.com>
2025-05-27 22:57:34 -0400

    removes apt-get clean line from golang Dockerfile to see if that...
    ...allows make to be found

--- go/Dockerfile.go
+++ go/Dockerfile.go
@@ -6,7 +6,6 @@ LABEL org.opencontainers.image.description="Go 1.24.3 Development Container"
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update && \
-    apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
 RUN curl -LO https://go.dev/dl/go1.24.3.linux-arm64.tar.gz && \
